19cf1467492f2ba2f4b20c4e8ad9280476bee880
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-01-11 11:55:58 +0000

    fix: whitespace and indentation issues as pep8 results

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ def welcome_screen():
     print("Welcome to BattleShip!")
     print("The goal of the game is to sink all of the ships.\n")
 
+
 instructions = """ 1. Select a board size either 5x5 or the original 8x8.
  2. Position your ships on the board. You can choose to position
     your ships either horizontally or vertically, but not diagonal.
@@ -12,29 +13,31 @@ instructions = """ 1. Select a board size either 5x5 or the original 8x8.
  4. If you hit a ship, you will be notified. If you miss, you will
     be notified as well.
  5. The game ends when all of the ships are sunk.
-    """   
-    
+    """
+
 welcome_screen()
 print(instructions)
+
 
 class battleShipBoard:
     """
     This class creates a board for the game.
     """
     def __init__(self, board_size):
-      """
-      This function sets the size of the board.
-      """
-      self.board_size = board_size
-      self.board = [[0] * board_size for _ in range(board_size)]
+        """
+        This function sets the size of the board.
+        """
+        self.board_size = board_size
+        self.board = [[0] * board_size for _ in range(board_size)]
 
     def print_board(self):
-      """
-      This function prints the board.
-      """
-      for row in self.board:
-         print(row)
-              
+        """
+        This function prints the board.
+        """
+        for row in self.board:
+            print(row)
+
+
 board_size = 5
 x = battleShipBoard(board_size)
 x.print_board()
